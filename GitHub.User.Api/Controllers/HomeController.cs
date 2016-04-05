188239283de2ec2bcb1e27267e69d7f1be95d1ae
Brighty28 <abright2012@gmail.com>
2016-04-05 10:44:00 +0100

    Initial Comment
    MVC Git Hub API demo app

---
+++ GitHub.User.Api/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GitHub.User.Api.Models;
+using Octokit;
+
+namespace GitHub.User.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        //
+        // GET: /SearchUsers/
+
+        [HttpPost]
+        public async Task<ActionResult> SearchUsers(string userName)
+        {
+            var github = new GitHubClient(new ProductHeaderValue("GitHubAPIApp"));
+
+            var user = await github.User.Get(userName);
+            if (ModelState.IsValid)
+            {
+
+                if (!user.Name.Contains(userName))
+                {
+                    ModelState.AddModelError("Names", "Error");
+                    return PartialView("_usersList");
+                }
+
+
+                // https://github.com/octokit/octokit.net/blob/master/docs/getting-started.md
+                //var user = "https://api.github.com/users" + "/" + user_name;
+
+            }
+            
+            var repos = await github.Repository.GetAllForUser(userName); //.Get(userName, userName);
+
+            var reposFiltered = repos.OrderByDescending(r => r.StargazersCount).Take(5);
+
+            var userDetails = new Models.User()
+            {
+                AvatarUrl = user.AvatarUrl,
+                Location = user.Location,
+                Name = userName != null ? user.Name : userName,
+                RepoList = reposFiltered.ToList()
+            };
+            return PartialView("_usersList", userDetails);
+        }
+
+    }
+}
---
+++ GitHub.User.Api/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Octokit;
+
+namespace GitHub.User.Api.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public List<Repository> RepoList { get; set; }
+
+    }
+
+    public class RepoListItem
+    {
+        public string RepoName { get; set; }
+
+        public string Url { get; set; }
+
+        public int? StargazersCount { get; set; }
+    }
+}
